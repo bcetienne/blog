955143827a356e730701c756679d8d3e5ce64265
admin <admin@iMac3-109.local>
2017-11-16 16:29:57 +0100

    Code Fragmentation

--- public/index.php
+++ public/index.php
@@ -1,18 +1,10 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Blog</title>
-</head>
-<body>
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
-</body>
-</html>
+<?php require_once 'templates/head.html'?><!-- Page Content -->
+    <div class="container">
+      <div class="row">
+        <?php require_once 'pages/article.php' ?>
+        <?php require_once 'templates/sidebar.php'?>
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container -->
+    <?php require_once 'templates/foot.html'?>
